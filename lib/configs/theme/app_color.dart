3c00d7cc9d406bc65cbd1194975e47b0b3693842
yorichi <hoangnguyen241003@gmail.com>
2024-10-22 16:13:32 +0700

    set up

---
+++ lib/configs/theme/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xFF317ef3);
+  static const Color blackColor = Color(0xFF1b262d);
+  static const Color whiteColor = Color(0xFFffffff);
+  static const Color greyColor = Color(0xFF252b32);
+}
